8f9f317ea586d88b273818cd20d0b760c7270d96
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-05-21 16:23:52 -0400

    deleting notes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -23,4 +23,17 @@ module.exports = function(app, db) {
       }
     });
   });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error': 'Could not delete'});
+      } else {
+        res.send(`Note ${id} deleted!`);
+      }
+    });
+  });
+
 };
