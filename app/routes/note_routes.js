e54da8de23dc71f4fa82fe83713a9b3b7872dfed
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-05-21 15:44:44 -0400

    setting up note route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,13 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
-    console.log(req.body)
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    })
     res.send('coucou');
   })
 };
