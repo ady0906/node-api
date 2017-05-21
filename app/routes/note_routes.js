a03e21d1934cbb4f9fd9bbb63d8a9160703c0faa
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-05-21 16:08:38 -0400

    writing get route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,14 @@
 module.exports = function(app, db) {
   app.get('/notes/:id', (req, res) => {
-    
-  })
+    const details = { '_id': '5921f2d935606e4f9d6c830e'};
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error': 'Something went down'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
 
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
