4310ed1664c9caca9c3621eeca613ccf954c6d9f
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-05-23 13:35:14 -0400

    adding put callback

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -37,7 +37,12 @@ module.exports = function(app, db) {
   });
 
   app.put('/notes/:id', (req, res) => {
-    
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      
+    })
   })
 
 };
